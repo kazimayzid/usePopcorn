fae34998c16309c3377b89a9c6fc4aaca3f8d93b
kazimayzid <kazimayzid208@gmail.com>
2026-01-08 23:56:20 +0600

    starRating done

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -10,45 +10,71 @@ const starContainerStyle = {
   display: "flex",
 };
 
-
-
-export default function StarRating({ maxRating = 5 ,color = "#fcc419", size = 48}) {
-  const [rating, setRating] = useState(0);
-  const [tempRating, setTempRating] = useState(0)
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating
+}) {
+  const [rating, setRating] = useState(defaultRating);
+  const [tempRating, setTempRating] = useState(0);
 
   const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-  color,
-  fontSize: `${size / 1.5}px`
-};
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  function handleRating(rating) {
+    setRating(rating + 1) 
+    onSetRating( rating + 1)
+  }
   return (
     <>
-      <div style={containerStyle}>
+      <div style={containerStyle} className={className}>
         <div style={starContainerStyle}>
           {Array.from({ length: maxRating }, (_, i) => (
-            <Star key={i} onClick={() => setRating(i + 1)} full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-            onHoverIn={() => setTempRating(i + 1)} onHoverOut= {() => setTempRating(0)} color={color} size={size}            />
+            <Star
+              key={i}
+              onClick={() => handleRating(i)}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
+              color={color}
+              size={size}
+            />
           ))}
         </div>
-        <p style={textStyle}>{tempRating || rating|| ""}</p>
+        <p style={textStyle}>
+          {messages.length === maxRating
+            ? messages[tempRating ? tempRating - 1 : rating - 1]
+            : tempRating || rating || ""}
+        </p>
       </div>
     </>
   );
 }
 
-
-
-function Star({ onClick, full, onHoverIn, onHoverOut, color, size}) {
+function Star({ onClick, full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
-  width: `${size}px`,
-  height: `${size}px`,
-  display: "block",
-  cursor: "pointer",
-};
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+  };
   return (
     <>
-      <span role="button" style={starStyle} onClick={onClick} onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
+      <span
+        role="button"
+        style={starStyle}
+        onClick={onClick}
+        onMouseEnter={onHoverIn}
+        onMouseLeave={onHoverOut}
+      >
         {full ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -77,4 +103,3 @@ function Star({ onClick, full, onHoverIn, onHoverOut, color, size}) {
     </>
   );
 }
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import StarRating from './StarRating.jsx'
 
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <App />
+    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating maxRating={6} color='green' size="100" className="test" defaultRating={3}/>
   </StrictMode>,
 )
